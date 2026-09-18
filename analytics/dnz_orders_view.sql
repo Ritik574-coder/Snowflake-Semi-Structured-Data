50fb47c0b2fab36bdf528d4c4b17d490114cf71a
ritik574-coder <ritik74820@gmail.com>
2026-09-18 22:16:21 +0530

    feat: adding script that createing the cuatomers, products, orders views that store non-sested data

--- analytics/dnz_orders_view.sql
+++ analytics/dnz_orders_view.sql
@@ -1,3 +1,5 @@
+-- creating view of orders table 
+CREATE OR REPLACE VIEW analytics.dnz_orders AS 
 SELECT
     i.value:product_id::STRING                             as product_id,
     i.value:product_name::STRING                           as product_name,
@@ -18,27 +20,27 @@ SELECT
     o.order_data:order_details:order_status::STRING        as order_status,
 
     o.order_data:payment:transaction_id::STRING            as transaction_id,   
-    o.order_data:payment:method::STRING                    as method,
+    o.order_data:payment:method::STRING                    as payment_method,
     o.order_data:payment:payment_status::STRING            as payment_status,
     o.order_data:payment:amount_paid::NUMBER(10,2)         as amount_paid,
 
-    o.order_data:payment:billing_address:country::STRING   as country,
-    o.order_data:payment:billing_address:state::STRING     as state,
-    o.order_data:payment:billing_address:city::STRING      as city,
-    o.order_data:payment:billing_address:zip::STRING       as zip,
-    o.order_data:payment:billing_address:street::STRING    as street,
+    o.order_data:payment:billing_address:country::STRING   as billing_address_country,
+    o.order_data:payment:billing_address:state::STRING     as billing_address_state,
+    o.order_data:payment:billing_address:city::STRING      as billing_address_city,
+    o.order_data:payment:billing_address:zip::STRING       as billing_address_zip,
+    o.order_data:payment:billing_address:street::STRING    as billing_address_street,
 
     o.order_data:shipping:tracking_number::STRING          as tracking_number,
     o.order_data:shipping:carrier::STRING                  as carrier,
     o.order_data:shipping:cost::NUMBER(10,2)               as cost,
     o.order_data:shipping:estimated_delivery::DATE         as estimated_delivery,
-    o.order_data:shipping:method::STRING                   as method,
+    o.order_data:shipping:method::STRING                   as shipping_method,
  
-    o.order_data:shipping:shipping_address:country::STRING as country,
-    o.order_data:shipping:shipping_address:state::STRING   as state,
-    o.order_data:shipping:shipping_address:city::STRING    as city,
-    o.order_data:shipping:shipping_address:zip::STRING     as zip,
-    o.order_data:shipping:shipping_address:street::STRING  as street
+    o.order_data:shipping:shipping_address:country::STRING as shipping_country,
+    o.order_data:shipping:shipping_address:state::STRING   as shipping_state,
+    o.order_data:shipping:shipping_address:city::STRING    as shipping_city,
+    o.order_data:shipping:shipping_address:zip::STRING     as shipping_zip,
+    o.order_data:shipping:shipping_address:street::STRING  as shipping_street
 FROM staging.orders as o 
 
 CROSS JOIN LATERAL FLATTEN(
---
+++ normalization/normalize_work.sql
@@ -0,0 +1,7 @@
+USE DATABASE semi_structured_db ; 
+select * from staging.customers; 
+
+
+SELECT  * FROM analytics.dnz_customers ;
+SELECT * FROM analytics.dnz_products ;
+SELECT * FROM analytics.dnz_orders ;
